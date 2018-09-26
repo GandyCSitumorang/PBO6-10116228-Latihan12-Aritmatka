e7d5ea857aa486f789938e225fd0671a1e2601b7
MY PC <MY PC@HOME-UEFI1TB>
2018-09-26 18:55:09 +0700

    Project GandyCSitumorang 10116228 PBO6

---
+++ src/pbo6/pkg10116228/latihan12/aritmatka/PBO610116228Latihan12Aritmatka.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6.pkg10116228.latihan12.aritmatka;
+
+/**
+ *
+ * @author Rizki Adam Kurniawan
+ */
+public class PBO610116228Latihan12Aritmatka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+    }
+    
+}
